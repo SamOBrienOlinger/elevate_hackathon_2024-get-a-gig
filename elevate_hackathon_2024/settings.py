f3968143d455d3b2badf60bb0ffa905e68050f39
SamOBrienOlinger <samtobo@yahoo.co.uk>
2024-10-18 16:45:32 +0000

    Install missing dependencies and update settings.py file

--- elevate_hackathon_2024/settings.py
+++ elevate_hackathon_2024/settings.py
@@ -22,6 +22,7 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
     "job-me-bc754484e6f7.herokuapp.com",
     "8000-shaander-elevatehackath-6m90ift8l4t.ws-eu116.gitpod.io",
+    "8000-samobrienol-elevatehack-pqnuhi1tzt5.ws-eu116.gitpod.io",
 ]
 
 
